9b20292febad7dee0b6c2a0984e7c35eda7c9c25
fr0zzy18 <67323938+fr0zzy18@users.noreply.github.com>
2026-02-14 18:24:40 +0200

    Players

--- frontend/rat-app-ui/src/app/components/players/players.component.ts
+++ frontend/rat-app-ui/src/app/components/players/players.component.ts
@@ -55,6 +55,26 @@ export class PlayersComponent implements OnInit {
     });
   }
 
+  classColors: Record<string, string> = {
+  'Death Knight': '#C41E3A',
+  'Demon Hunter': '#A330C9',
+  'Druid': '#FF7C0A',
+  'Evoker': '#33937F',
+  'Hunter': '#AAD372',
+  'Mage': '#3FC7EB',
+  'Monk': '#00FF98',
+  'Paladin': '#F48CBA',
+  'Priest': '#FFFFFF',
+  'Rogue': '#FFF468',
+  'Shaman': '#0070DD',
+  'Warlock': '#8788EE',
+  'Warrior': '#C69B6D'
+};
+
+getClassColor(className: string): string {
+  return this.classColors[className] || '#ffffff';
+}
+
   onCategoryChange(category: string): void {
     this.selectedCategory = category;
     this.loadPlayers(); // Reload players for the selected category
